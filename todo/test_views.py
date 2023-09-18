d3b943880aef324b847e36378dd6a6e0e1492748
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-09-18 19:16:27 +0200

    Setup environment variables and automatic depolyment

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,10 +1,9 @@
 from django.test import TestCase
 from .models import Item
 
-# Create your tests here.
-
 
 class TestViews(TestCase):
+
     def test_get_todo_list(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
